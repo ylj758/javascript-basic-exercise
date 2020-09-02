800b110cdbfa41ec37d5c250d76d3905428afd1e
ylj <758766951@qq.com>
2020-09-02 15:40:54 +0800

    Complete exercise_04.

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,14 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array == null) throw new Error('Creating list from undefined array');
+  if (array.length === 0) throw new Error('Creating list from empty array');
+  const head = { value: array[0], next: null };
+  let temp = head;
+  for (let i = 1; i < array.length; i += 1) {
+    temp.next = { value: array[i], next: null };
+    temp = temp.next;
+  }
+  return head;
+  // throw new Error('Please delete this line and implement the function');
 }
