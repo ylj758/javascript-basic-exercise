f1a52cb0fd4a4b2044030f7fb284b270aac716ec
ylj <758766951@qq.com>
2020-09-02 14:54:14 +0800

    Complete exercise_02.

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,16 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (string == null || string === '') return 0;
+  if (prediction) {
+    let count = 0;
+    Object.values(string.split('')).forEach((value) => {
+      if (prediction(value)) {
+        count += 1;
+      }
+    });
+    return count;
+  }
+  return string.length;
+  // throw new Error('Please delete this line and implement the function');
 }
