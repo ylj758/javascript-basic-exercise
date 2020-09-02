f046597fb717f375cafe7ddc6bc7cc7ff44714d5
ylj <758766951@qq.com>
2020-09-02 11:51:19 +0800

    Complete exercise_01.

--- src/exercise_01/numberFormatter.js
+++ src/exercise_01/numberFormatter.js
@@ -7,6 +7,10 @@ export default function formatNumber(number, option) {
   //
   // * Please implement the function and pass all the tests in format_number_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  const newNumber = (Math.round(number * 100) / 100).toFixed(2);
+  if (option) {
+    return `$ ${newNumber}`;
+  }
+  return newNumber.toString();
+  // throw new Error('Please delete this line and implement the function');
 }
