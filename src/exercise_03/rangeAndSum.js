f4b09c83e67640e1d4c27ab5a875cbd0fcd7313d
ylj <758766951@qq.com>
2020-09-02 15:14:47 +0800

    Complete exercise_03.

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -6,8 +6,20 @@ export function range(start, end) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (start === end) return [];
+  const array = [];
+  if (start < end) {
+    for (let i = start; i < end; i += 1) {
+      array.push(i);
+    }
+  }
+  if (start > end) {
+    for (let i = start; i > end; i -= 1) {
+      array.push(i);
+    }
+  }
+  return array;
+  // throw new Error('Please delete this line and implement the function');
 }
 
 export function sum(...numbers) {
@@ -17,6 +29,7 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (numbers.length === 0) return 0;
+  return numbers.reduce((x, y) => x + y);
+  // throw new Error('Please delete this line and implement the function');
 }
